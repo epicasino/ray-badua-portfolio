1bd544ab7928ae71646683e44cdcd4576a3dbcb5
Ray Badua <rayjonathanbadua@gmail.com>
2023-07-31 17:19:29 -0700

    updated about me portfolio section

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,8 +1,6 @@
 import "../components/main/aboutMe/aboutMe.css";
 
 export default function AboutMe() {
-  
-
   return (
     <>
       <div id="aboutMe">
@@ -11,9 +9,15 @@ export default function AboutMe() {
           <p>
             Hello! I&#39;m a full-stack engineer with expertise in MongoDB,
             Express, React, and NodeJS. I&#39;ve been a programmer for 2 years,
-            with notable proficiency in JavaScript.
+            with notable proficiency in back-end relational and non-relational
+            databases. Able to build scalable web applications using modern web
+            technologies.
           </p>
-          <p>Besides coding, I love playing the guitar and rock climbing!</p>
+          <br />
+          <p>
+            Outside of coding, You&#39;ll catch me rock climbing, spending time
+            with my two dogs, or singing with my guitar or piano!
+          </p>
         </div>
         <img
           className="aboutMeImg fadeIn"
