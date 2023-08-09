42d95aa3501ec41497f368a2d354060ae718aee3
Ray Badua <rayjonathanbadua@gmail.com>
2023-08-09 15:39:12 -0700

    added project 3 into portfolio

--- src/components/main/portfolio/projects/sideProjects/sideProjectArray.js
+++ src/components/main/portfolio/projects/sideProjects/sideProjectArray.js
@@ -1,5 +1,14 @@
 export const sideProjectArray = [
   {
+    projectName: 'Fishing Forecast',
+    projectTech: 'MongoDB, ExpressJS, React, NodeJS',
+    description:
+      'A full-stack web application that gets current information on weather, along with a weekly tide-chart to plan your next fishing trip in San Diego.',
+    github: 'https://github.com/epicasino/fishing-forecast',
+    heroku: 'https://github.com/epicasino',
+    imgUrl: '../../../../assets/images/bi-background-black.png',
+  },
+  {
     projectName: `BallDontLie`,
     projectTech: `NodeJS, ExpressJS, MySQL`,
     description: `A simple, sleek, and functional NBA player search application to track your favorite basketball players in the NBA. Search for players and teams in the NBA and get their up-to-date information.`,
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,7 @@
-import "../components/main/portfolio/portfolio.css";
-import MainProject from "../components/main/portfolio/projects/MainProject";
-import SideProject from "../components/main/portfolio/projects/sideProjects/SideProject";
-import { sideProjectArray } from "../components/main/portfolio/projects/sideProjects/sideProjectArray";
+import '../components/main/portfolio/portfolio.css';
+import MainProject from '../components/main/portfolio/projects/MainProject';
+import SideProject from '../components/main/portfolio/projects/sideProjects/SideProject';
+import { sideProjectArray } from '../components/main/portfolio/projects/sideProjects/sideProjectArray';
 
 function Portfolio() {
   return (
@@ -13,12 +13,13 @@ function Portfolio() {
 
         <MainProject
           props={{
-            title: "Fishing Forecast",
-            tech: "MongoDB, ExpressJS, React, NodeJS",
-            description: "A full-stack web application that gets current information on weather, along with a weekly tide-chart to plan your next fishing trip in San Diego.",
-            github: "https://github.com/epicasino/fishing-forecast",
-            heroku: "https://github.com/epicasino",
-            imgUrl: "../../../../assets/images/bi-background-black.png",
+            title: 'Day Tripper',
+            tech: 'MongoDB, ExpressJS, React, NodeJS',
+            description:
+              'A full-stack web application that allows you to create an account, create a road trip, and save to your account. Trip data supplied by the Google Platform API.',
+            github: 'https://github.com/epicasino/daytripper',
+            heroku: 'https://daytripper-app-1577e78f3877.herokuapp.com/',
+            imgUrl: '../../../../assets/images/daytripper.JPG',
           }}
         />
         {sideProjectArray.map((project) => (
