6833b53a0d8d270941ebf4e6e7cc14e743e1f537
Ray Badua <rayjonathanbadua@gmail.com>
2023-09-22 02:43:05 -0700

    starter styling, sidebar

---
+++ src/app/components/sidebar/Sidebar.tsx
@@ -0,0 +1,18 @@
+export default function Sidebar() {
+  return (
+    <aside className="w-1/2 flex flex-col p-10">
+      <header className="text-neutral-50 flex flex-col gap-3">
+        <h1 className="text-7xl font-bold">Ray Badua</h1>
+        <h3 className="text-2xl font-semibold">Full Stack Web Developer</h3>
+        <h5 className="text-xl font-light">
+          I build efficient and scalable web applications using modern web
+          technologies.
+        </h5>
+        <hr />
+      </header>
+      <nav>
+
+      </nav>
+    </aside>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Sidebar from './components/sidebar/Sidebar';
+
 export default function Home() {
   return (
-    <main>
-      <div></div>
+    <main className="bg-neutral-950 mx-auto w-auto min-h-screen flex justify-between gap-6 px-24">
+      <Sidebar />
+      <div className="w-1/2 bg-white"></div>
     </main>
   );
 }
+
+// #161616
